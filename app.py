f8723956e4b502cd80cb980fcdb15a0d39656b7f
elieno <Elijahenoidem@yahoo.com>
2023-04-15 22:43:37 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "I am almost a Devops Engineer and ready to get into the job market!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
